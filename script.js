ba8fa6af523989028503a8309f5086dfa4226cc1
FathiirMaulanaa <153278914+fathiirmaulanaa@users.noreply.github.com>
2024-09-26 13:05:52 +0700

    commit first

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Change Navbar Background on Scroll
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});

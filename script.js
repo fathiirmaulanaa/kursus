69f9184e278f3d6c1bf26733f3dd22699d78c7e6
fathiirmaulanaa <fath0250@gmail.com>
2024-10-08 00:47:20 +0700

    style

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
 });
 
 // Change Navbar Background on Scroll
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('header');
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
   if (window.scrollY > 50) {
-    header.classList.add('scrolled');
+    header.classList.add("scrolled");
   } else {
-    header.classList.remove('scrolled');
+    header.classList.remove("scrolled");
   }
 });
+
+function ubahIconbar() {
+  const icon = document.getElementById("icon");
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+  }
+  else {
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  }
+}
+
+
